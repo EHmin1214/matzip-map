bd9294350572d007b3a3ea5918e07478095bb15a
박태민 <ehmin@bagtaemin-ui-MacBookAir.local>
2026-03-27 01:26:02 -0700

    add production URL to CORS

--- backend/main.py
+++ backend/main.py
@@ -37,7 +37,7 @@ app = FastAPI(
 # CORS 설정 (React 개발 서버 허용)
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000"],
+    allow_origins=["http://localhost:3000", "http://127.0.0.1:3000", "https://restaurant-map-rosy.vercel.app",],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
