e821e3edddde55587012c0a6a4a33422b064191c
박태민 <ehmin@bagtaemin-ui-MacBookAir.local>
2026-03-28 22:08:58 -0700

    feat: 로그인 화면, 하단 탭바 추가

--- backend/main.py
+++ backend/main.py
@@ -37,6 +37,8 @@ app.add_middleware(
         "http://localhost:3000",
         "http://127.0.0.1:3000",
         "https://restaurant-map-rosy.vercel.app",
+        "https://matzip-map.vercel.app",
+
     ],
     allow_credentials=True,
     allow_methods=["*"],
